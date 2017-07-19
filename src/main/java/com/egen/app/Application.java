ad9458b160910179b238fe171033ce5f5323e516
rajkumar <rajkumar@localhost.localdomain>
2017-07-19 02:48:52 -0400

    egen test

--- src/main/java/com/egen/app/Application.java
+++ src/main/java/com/egen/app/Application.java
@@ -7,11 +7,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableAutoConfiguration
 @EntityScan("com.egen.entities")
 @ComponentScan(basePackages = {"com.egen.services", "com.egen.resources"})
 @EnableJpaRepositories("com.egen.dao")
+@EnableSwagger2
 public class Application {
 
 	public static void main(String[] args) {
